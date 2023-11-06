defbe0996d9355fe1fb8abfd274fb5ca7cc6f487
Ahmet Er <ahmet@gmail.com>
2023-11-06 20:52:44 +0300

    project 3 done

--- DaireCizme/Program.cs
+++ DaireCizme/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Kullanıcıdan alınan boyut bilgisine göre console'a Üçgen çizen uygulamayı yazınız.");
+
+char exit = 'c';
+
+while (exit != 'e')
+{
+    System.Console.Write("Circle radius: ");
+
+    try
+    {
+        int radius = Convert.ToInt32(Console.ReadLine());
+
+        DrawTriangle(radius);
+
+        Console.Write("If you want to exit, press 'e'; if you want to continue, press'c':");
+        exit = Convert.ToChar(Console.ReadLine());
+    }
+    catch (System.Exception ex)
+    {
+        System.Console.WriteLine(ex);
+    }
+}
+
+
+void DrawTriangle(int radius)
+{
+    for (global::System.Int32 i = -radius; i <= radius; i++)
+    {
+        for (global::System.Int32 j = -radius; j <= radius; j++)
+        {
+            if (i * i + j * j <= radius * radius)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
